333d71340232596a0fa88df72966fa3ce3b35352
nitin-rajesh <nitin.rajesh@outlook.com>
2025-04-19 21:34:52 +0530

    GRU decoder added

---
+++ img_caption/datasetloader.py
@@ -0,0 +1,24 @@
+class FlickrDataset(Dataset):
+    def __init__(self, root_folder, captions_file, vocab, transform=None):
+        self.root_folder = root_folder
+        self.df = [line.strip().split(',') for line in open(captions_file, 'r') if len(line.strip()) > 0]
+        self.transform = transform
+        self.vocab = vocab
+        self.captions = [line[1] for line in self.df]
+        self.vocab.build_vocab(self.captions)
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, index):
+        img_id, caption = self.df[index]
+        img_path = os.path.join(self.root_folder, img_id)
+        image = Image.open(img_path).convert("RGB")
+        if self.transform:
+            image = self.transform(image)
+
+        numericalized_caption = [self.vocab.stoi["<SOS>"]]
+        numericalized_caption += self.vocab.numericalize(caption)
+        numericalized_caption.append(self.vocab.stoi["<EOS>"])
+        
+        return image, torch.tensor(numericalized_caption)
---
+++ img_caption/gru_decoder.py
@@ -0,0 +1,37 @@
+class DecoderGRU(nn.Module):
+    def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
+        super().__init__()
+        self.embedding = nn.Embedding(vocab_size, embed_size)
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.embed_size = embed_size
+        
+        # GRU parameters
+        self.Wz = nn.Linear(embed_size + hidden_size, hidden_size)
+        self.Wr = nn.Linear(embed_size + hidden_size, hidden_size)
+        self.Wh = nn.Linear(embed_size + hidden_size, hidden_size)
+        
+        self.fc_out = nn.Linear(hidden_size, vocab_size)
+
+    def forward(self, features, captions):
+        batch_size, seq_len = captions.size()
+        embeddings = self.embedding(captions)  # (B, T, E)
+
+        h = features  # initial hidden state from image
+
+        outputs = []
+        for t in range(seq_len):
+            x_t = embeddings[:, t, :]
+            combined = torch.cat((x_t, h), dim=1)
+            
+            z = torch.sigmoid(self.Wz(combined))
+            r = torch.sigmoid(self.Wr(combined))
+            combined_reset = torch.cat((x_t, r * h), dim=1)
+            h_tilde = torch.tanh(self.Wh(combined_reset))
+            h = (1 - z) * h + z * h_tilde
+
+            out = self.fc_out(h)
+            outputs.append(out)
+
+        outputs = torch.stack(outputs, dim=1)
+        return outputs
